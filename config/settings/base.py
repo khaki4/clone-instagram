07c042fcbc5f23f53e2e9b6ef9a3e58a9ed1bdf8
pi3rrot <4dakto@gmail.com>
2018-05-17 18:16:15 +0900

    add rest_framework

--- config/settings/base.py
+++ config/settings/base.py
@@ -66,7 +66,7 @@ THIRD_PARTY_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'rest_framework',
+    'rest_framework', # REST framework
 ]
 LOCAL_APPS = [
     'instagram.users.apps.UsersConfig',
