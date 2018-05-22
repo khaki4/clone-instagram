c4eb976b04df147c21b5ece2c1567a2db82c3670
pi3rrot <4dakto@gmail.com>
2018-05-22 21:40:15 +0900

    unlike an Image [delete]

--- instagram/images/views.py
+++ instagram/images/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from . import models, serializers
+
 
 
 class Feed(APIView):
@@ -32,16 +34,30 @@ class LikeImage(APIView):
 
     def get(self, request, image_id, format=None):
 
+        user = request.user
+
         try:
             found_image = models.Image.objects.get(id=image_id)
         except models.Image.DoesNotExist:
-            return Response(status=404)
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        new_like = models.Like.objects.create(
-            creator=request.user,
-            image=found_image
-        )
+        try:
+            pre_existing_like = models.Like.objects.get(
+                creator=user,
+                image=found_image
+            )
 
-        new_like.save()
+            pre_existing_like.delete()
 
-        return Response(status=200)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except models.Like.DoesNotExist:
+
+            new_like = models.Like.objects.create(
+                creator=user,
+                image=found_image
+            )
+
+            new_like.save()
+
+        return Response(status=status.HTTP_201_CREATED)
