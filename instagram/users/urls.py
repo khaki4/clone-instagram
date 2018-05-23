5e919132c0c1b352e8103df3a326990bc86e951f
pi3rrot <4dakto@gmail.com>
2018-05-23 22:57:39 +0900

    Get people to follow on Explore [GET]

--- instagram/users/urls.py
+++ instagram/users/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 app_name = "users"
 urlpatterns = [
-    path("", view=views.UserListView.as_view(), name="list"),
-    path("~redirect/", view=views.UserRedirectView.as_view(), name="redirect"),
-    path("~update/", view=views.UserUpdateView.as_view(), name="update"),
-    path(
-        "<str:username>",
-        view=views.UserDetailView.as_view(),
-        name="detail",
+    url(
+        regex=r'^explore/$',
+        view=views.ExploreUsers.as_view(),
+        name='explore_users'
     ),
 ]
