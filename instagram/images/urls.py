c8da612aa3caa6d100d50b2ec23f2fc8f97769f9
pi3rrot <4dakto@gmail.com>
2018-05-27 13:33:17 +0900

    Delete any comment from my photos [DELETE]

--- instagram/images/urls.py
+++ instagram/images/urls.py
@@ -10,22 +10,27 @@ urlpatterns = [
         name='feed'
     ),
     url(
-        regex=r'^(?P<image_id>\w+)/like/',
+        regex=r'^(?P<image_id>[0-9]+)/like/$',
         view=views.LikeImage.as_view(),
         name='like_image'
     ),
     url(
-        regex=r'^(?P<image_id>\w+)/unlike/',
+        regex=r'^(?P<image_id>[0-9]+)/unlike/$',
         view=views.UnLikeImage.as_view(),
-        name='unlike_image'
+        name='like_image'
     ),
     url(
-        regex=r'^(?P<image_id>\w+)/comments/',
+        regex=r'^(?P<image_id>[0-9]+)/comments/$',
         view=views.CommentOnImage.as_view(),
         name='comment_image'
     ),
     url(
-        regex=r'comments/(?P<comment_id>[0-9]+)/$',
+        regex=r'^(?P<image_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$',
+        view=views.ModerateComments.as_view(),
+        name='comment_image'
+    ),
+    url(
+        regex=r'^comments/(?P<comment_id>[0-9]+)/$',
         view=views.Comment.as_view(),
         name='comment'
     ),
@@ -33,5 +38,5 @@ urlpatterns = [
         regex=r'^search/$',
         view=views.Search.as_view(),
         name='search'
-    ),
+    )
 ]
