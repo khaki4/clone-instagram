0881c267eba880de3bff7b2336a0fc0d7d881bb1
pi3rrot <4dakto@gmail.com>
2018-05-27 14:24:01 +0900

    Edit a photo [PUT] - partial update

--- instagram/images/serializers.py
+++ instagram/images/serializers.py
@@ -72,3 +72,15 @@ class ImageSerializer(serializers.ModelSerializer):
             'created_at'
         )
 
+
+class InputImageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'file',
+            'location',
+            'caption',
+        )
+
+
--- instagram/images/views.py
+++ instagram/images/views.py
@@ -51,7 +51,7 @@ class LikeImage(APIView):
         serializer = user_serializers.ListUserSerializer(users, many=True)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-    
+
 
     def post(self, request, image_id, format=None):
 
@@ -200,6 +200,28 @@ class ImageDetail(APIView):
         serializer = serializers.ImageSerializer(image)
 
         return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, image_id, format=None):
+        # 자신이 생성한 이미지만 update가능.
+
+        user = request.user
+
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+        except models.Image.DoesNotExist:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+        else:
+
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
