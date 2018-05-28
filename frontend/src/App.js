f55d17beae32e5040444a941b81456a4a1ac9d35
pi3rrot <4dakto@gmail.com>
2018-05-28 23:45:43 +0900

    Setting Up the Redux Store with Multiple Reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,7 @@ import styles from './App.scss';
 class App extends Component {
   render() {
     return (
-      <div className={styles.App}>
-        <header className={styles.App__header}>
-          <img src={logo} className={styles.App__logo} alt="logo" />
-          <h1 className={styles.App__title}>Welcome to React</h1>
-        </header>
-        <p className={styles.App__intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className={styles.App} />
     );
   }
 }
