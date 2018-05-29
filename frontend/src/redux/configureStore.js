68604adad35cdad4e7bfdde4b4852152c3e2b468
pi3rrot <4dakto@gmail.com>
2018-05-29 22:52:35 +0900

    Multi Language with Redux

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -3,6 +3,7 @@ import thunk from 'redux-thunk';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { i18nState } from 'redux-i18n';
 import users from 'redux/modules/users';
 
 const env = process.env.NODE_ENV;
@@ -33,6 +34,7 @@ if (env === 'development') {
 const reducer = combineReducers({
   users,
   routing: routerReducer,
+  i18nState,
 });
 
 export { history };
