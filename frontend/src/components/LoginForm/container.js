b7ee9770de4574cd9ef8f21144f4e9ed01d44188
pi3rrot <4dakto@gmail.com>
2018-05-31 23:51:27 +0900

    FB login with redux

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import LoginForm from './presenter'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import LoginForm from "./presenter";
 
 class Container extends Component {
   state = {
-    username: '',
-    password: ''
-  }
-
+    username: "",
+    password: ""
+  };
+  static propTypes = {
+    facebookLogin: PropTypes.func.isRequired
+  };
   render() {
     const { username, password } = this.state;
-    return <LoginForm
-      handleInputChange={this._handleInputChange}
-      handleSubmit={this._handleSubmit}
-      handleFacebookLogin={this._handleFacebookLogin}
-      usernameValue={username}
-      passwordValue={password}
-    />
+    return (
+      <LoginForm
+        handleInputChange={this._handleInputChange}
+        handleSubmit={this._handleSubmit}
+        usernameValue={username}
+        passwordValue={password}
+        handleFacebookLogin={this._handleFacebookLogin}
+      />
+    );
   }
   _handleInputChange = event => {
     const { target: { value, name } } = event;
     this.setState({
       [name]: value
-    })
+    });
   };
   _handleSubmit = event => {
     event.preventDefault();
-    // redux will be here
-  }
+  };
   _handleFacebookLogin = response => {
-    console.log(response);
-  }
+    const { facebookLogin } = this.props;
+    facebookLogin(response.accessToken);
+  };
 }
 
 export default Container;
--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -4,6 +4,23 @@
 
 // actions creators
 
+// API actions
+
+const facebookLogin = (access_token) => (dispatch) => {
+  return fetch('/users/login/facebook/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(({
+      access_token
+    }))
+  })
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+};
+
 // initial state
 const initialState = {
   isLogedin: localStorage.getItem('jwt') || false
@@ -21,6 +38,12 @@ const reducer = (state = initialState, action) => {
 // reducer functions
 
 // exports
+
+const actionCreators = {
+  facebookLogin
+};
+
+export { actionCreators }
 
 // reducer export
 export default reducer;
